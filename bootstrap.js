bb203525e104856cdac5273c78329d38f2dc9684
Jet Simon <jetjdsimon@gmail.com>
2021-05-19 11:27:17 -0700

    actually stop scrolling on input on phones ?

--- bootstrap.js
+++ bootstrap.js
@@ -16,9 +16,13 @@
     // add input handler forward
     document.body.addEventListener('keydown', (event) => { event.preventDefault(); container.processInput(event.key.toLowerCase()) })
     document.body.addEventListener('touchstart', (_) => { event.preventDefault(); container.processInput(' ') })
-    
+
     handle.addEventListener('touchstart', (event) => {event.preventDefault(); container.processTouchInputDown(event) })
     handle.addEventListener('touchend', (event) => {event.preventDefault(); container.processTouchInputUp(event) })
+
+    //These are in an attempt to stop scrolling on user input
+    handle.addEventListener('touchmove', (event) => {event.preventDefault();})
+    handle.addEventListener('touchcancel', (event) => {event.preventDefault();})
   
     // start game
     await container.start()
